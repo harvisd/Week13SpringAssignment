9f53a98cd919e87593b2b5b1712a880bde937ed6
Jarvey D <harvisd@hotmail.com>
2022-06-01 06:13:40 -0400

    Week 13 Spring Assignment

---
+++ jeep-sales/src/main/java/com/promineotech/jeep/JeepSales.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.promineotech.jeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author harvi
+ *
+ */
+@SpringBootApplication
+public class JeepSales {
+  
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(JeepSales.class, args);
+  }
+
+}
---
+++ jeep-sales/src/main/java/com/promineotech/jeep/controller/DefaultJeepSalesController.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.promineotech.jeep.controller;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.RestController;
+import com.promineotech.jeep.entity.Jeep;
+import com.promineotech.jeep.entity.JeepModel;
+
+/**
+ * @author harvi
+ *
+ */
+@RestController
+public class DefaultJeepSalesController implements JeepSalesController {
+
+  @Override
+  public List<Jeep> fetchJeeps(JeepModel model, String trim) {
+
+    return null;
+  }
+
+}
